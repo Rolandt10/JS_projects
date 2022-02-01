ffc7e943c3c82a516bf1009941cae50ec42097c0
Roland Toth <troland703@gmail.com>
2022-02-01 16:25:11 +0100

    Move App works

--- movie_app/script.js
+++ movie_app/script.js
@@ -2,14 +2,47 @@ const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_URL = "https://api.themoviedb.org/3/search/movie?api_key=513f332206c6de1e2f0a093bc2360b9a&query=";
 
+const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
 async function getMovies(url) {
     const res = await fetch(url);
     const data = await res.json();
+    showMovies(data.results);
+}
 
-    console.log(data);
+getMovies(API_URL);
+
+function showMovies(movies) {
+    main.innerHTML = "";
+    movies.forEach(movie => {
+        const { title, poster_path, vote_average, overview } = movie;
+
+        const movieElement = document.createElement("div");
+        movieElement.classList.add("movie");
+        movieElement.innerHTML = `
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>Overview</h3>
+            ${overview}
+        </div>`;
+    main.appendChild(movieElement);
+    });
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
 }
 
 form.addEventListener("submit", e => {
